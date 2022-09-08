a2ff774df106dd876da4d00eb07efd86682e07ab
SOULARD Florian <F.SOULARD@arvalis.fr>
2022-09-08 17:06:15 +0200

    Update du jour : Après entretien avec Gaethan, il s'agit que pour la suite de mon travail, je vais donc travailler avec des rasters de résolution 1cm*1cm. J'ai donc modifié le script pour que l'on ai pour les données de Profils Racinaire une génération de raster aux propriétés adéquats. Pour l'instant c'est que sur un exemple, mais c'est généralisable.

--- main.R
+++ main.R
@@ -7,39 +7,37 @@ source("script-chargement-paquets.R")
 
 ### Création Fichier Test 
 
-test <- read.csv2("2019-Nimes-BleDur_Fourques_depthed.csv", sep = ";" )
+test <- rast(xmin = 0, xmax = 100, ymin = 0, ymax = 200, res = 1)
 
-test <- test %>%
-  filter(tube == "T4", date == "30.04.19") %>% 
-  select(dpi, width, height, surface0, surface1) %>% 
-  mutate(surface = surface0 + surface1) %>%
-  mutate(pourc = surface1 / surface)
+values(test) <- sample(x = 1:2000, replace = T, size = 13) 
 
-surface.tot = sum(test$surface) 
-long = sum(test$height)
-larg = unique(test$width)
-pourc = mean(test$pourc)
+test
+plot(test, type = "class")
 
-test <- rast(ncol = larg, nrow = long, xmin = 0, xmax = 100, ymin = -200, ymax = 0)
+### Chargement Donnees Profils 
 
-values(test) <- 1
+### Chargement Donnees Miniriz
+
+test <- read.csv2("17 0N-VLB-2021-1-data.csv")
+# Je charge mon fichier source
+# Je retire les cases inutiles présentes dans le template de base servant de légendes et je ne garde que les données pures
+
+test$X <- NULL
+test$prof...largeur <- NULL
+test$fichier <- NULL
+
+test <- rast(x = as.matrix(test))
+# Je crée un raster à partir de ma matrice de données stockée en .csv
+
+terra::ext(test) <- c(0, 2 * ncol(test), 0, 2 * nrow(test))
+test <- terra::disagg(x = test, fact = 2, method = "near")
+# Avec ces deux lignes, j'agrandis mon raster de deux fois en taille en gardant la même résolution. 
+# L'objetcif était de passer de 1 pixel = 4cm² à 1 pixel = 1cm². 
+# Comme ça la résolution entre les rasters issu des données de profils seront de la même résolution que les rasters issu des données de miniriz.
+# En plus ce sera plus simple pour travailler. 
 
 test
 plot(test)
-
-### 
-
-### 
-
-### Rasterisation 
-
-raster.point <- rasterize(x = point, y = raster, background = 0)
-# étape de rasterisation des vecteurs relevés
-
-
-
-
-
 
 
 
