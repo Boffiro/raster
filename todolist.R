18747caa48ecb13c35c8ae1a7268dc22ce6615fd
Boffiro <71046033+Boffiro@users.noreply.github.com>
2022-10-17 23:58:09 +0200

    todo list pour le 18-10-2022

---
+++ todolist.R
@@ -0,0 +1,17 @@
+# To do list  : 
+
+# Calculer indicateurs : 
+# - RMSE
+# - Accuracy
+# - Biais
+# - Efficiency
+# et autres
+
+rmse <- sqrt(mean((data$actual - data$predicted)^2))
+
+# envoyer demandes congès
+
+# demander à Benoit pour optim
+
+# etudier pour res 2 et seulement res 2 dan un premier temps 
+# --> bien étudier et bien estimer à terme l'erreur systématique que l'on a avec la reclassification quali vs quanti
